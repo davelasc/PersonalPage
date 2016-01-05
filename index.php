132dbb2ef3759d152c2c8066f3dacdea712af056
David Alfonso Velasco Sedano <velascos.davida@gmail.com>
2016-01-04 22:40:57 -0600

    Created index.php (currently has basic interface), created the greetingBlock.php (has TO-DO instructions)

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php 
+	/*
+	*Last update: January 4, 2016
+	*Author: David Velasco
+	*Contains: Personal web page
+	/**/
+	
+	define ("ABS_PATH", $_SERVER['DOCUMENT_ROOT']);
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>David's Portfolio</title>
+        <meta charset="utf-8" />
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+        <script type="text/javascript" src="jquery.min.js" > </script>
+        <link rel="stylesheet" type="text/css" href="css/styles.css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		
+    </head>
+	
+    <body data-spy="scroll" data-target=".navbar-collapse">
+    	
+    	<div class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<!-- TO-DO: Add a home page icon if possible, check this at the end -->
+					<a class="navbar-brand" href="#divAboutMe">David Velasco's Portfolio </a>
+					<button type="button" class="navbar-toggle" data-toggle="collapse" 
+					data-target=".navbar-collapse">
+						<span class="sr-only"> Toggle Navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+				<!-- TO-DO: Update menu to reflect version -->
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav" id="myMenu">
+						<li><a href="#divEducation">Education</a></li>
+						<li><a href="#divExperience">Experience</a></li>
+						<li><a href="#divPortfolio">Portfolio</a></li>
+						<li><a href="#divContactMe">Contact Me</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+    
+    	<!-- TO-DO: Finish greetingBlock -->
+    	<?php 
+    		/*TO-DO: Ensure that patch works properly*/
+    		include (ABS_PATH . "/private_test_html/pageDivisions/greetingBlock.php"); 
+    	?>
+    </body>
+</html>
---
+++ pageDivisions/greetingBlock.php
@@ -0,0 +1,19 @@
+<?php
+	/*
+	*Last update: January 4, 2016
+	*Author: David Velasco
+	*Contains: Professional photo, full name, inspiration qoute
+	/**/
+?>
+
+<div id="aboutMeContainer" class="container contentContainer">
+	<div class="row" id="divAboutMe">
+		<!-- TO-DO: Change the background image or the size of it-->
+		<div class="col-md-6 box">
+			<!-- TO-DO: Add picture with general information about me under it-->	
+		</div>
+		<div class="col-md-6 box">
+			<!-- TO-DO: Add inspirational quote that can define me-->
+		</div>	
+	</div>
+</div>
